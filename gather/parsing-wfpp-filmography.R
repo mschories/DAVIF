f2450ec8ca94f8b458f9e1a7c8ff834435f746ef
Martina Schories <20225984+mschories@users.noreply.github.com>
2024-07-30 16:54:40 +0200

    Update parsing-wfpp-filmography.R

--- gather/parsing-wfpp-filmography.R
+++ gather/parsing-wfpp-filmography.R
@@ -1,20 +1,24 @@
 library(tidyverse)
 
-path_to_files <- "data/DataViz/Daten_WFPP/Filmografien_2024_0521/"
+path_to_files <- "../data/DataViz/Daten_WFPP/Filmografien_2024_0521/"
 
-file_list <- list.files("data/DataViz/Daten_WFPP/Filmografien_2024_0521/")
+file_list <- list.files("../data/DataViz/Daten_WFPP/Filmografien_2024_0521/")
 
 df_wfpp_filmography_wide <- map_df(file_list, function(file){
   df <- read_csv(paste0(path_to_files, file), show_col_types = FALSE, col_types = cols(.default = col_character())) %>% 
     mutate(filename = file)
 })
 
-id_cols <- c("filename", "Title", "Archives", "archives", "unfinished", "uncertainty", "Release_years", "Release_Years", "production_company", "Production_Company", "Production_Country")
+id_cols <- c("filename", "Title", "archives", "unfinished", "uncertainty", "release_years", "production_company", "Production_Country")
 
 df_wfpp_filmography_long <- df_wfpp_filmography_wide %>% 
-  select(!!!id_cols, everything()) %>% #View()
+  mutate(archives = ifelse(is.na(Archives), archives, Archives),
+         release_years = ifelse(is.na(Release_years), Release_Years, Release_years),
+         production_company = ifelse(is.na(production_company), Production_Company, production_company)) %>% 
+  select(!!!id_cols, everything(), -Release_years, -Release_Years, -Archives, -Production_Company, -`...32`) %>% #View()
   pivot_longer(., cols = "Director":last_col(), names_to = "role", values_to = "value") %>% 
-  filter(!is.na(value))
+  filter(!is.na(value)) %>% 
+  mutate(role = tolower(role))
   
 
 nr_persons <- df_wfpp_filmography_long %>% select(value) %>% distinct() %>% 
@@ -29,7 +33,7 @@ df_wfpp_filmography_long_persons <- df_wfpp_filmography_long %>%
          persons_bio = str_remove(filename, "Filmografie_") %>% str_remove(., "_v.*$")
          )
 
-df_wfpp_local_wd_ids <- read_csv("data/wikidata/wfpp-22-with-wikidata-ids.csv") %>% 
+df_wfpp_local_wd_ids <- read_csv("../data/wikidata/wfpp-22-with-wikidata-ids.csv") %>% 
   mutate(name = ifelse(is.na(name), name_person, name))
 
 #### Jetzt die filmografien mergen mit den biografischen daten -------------------------------------
@@ -58,7 +62,7 @@ missed_files_for_export <- df_wfpp_filmography_long_persons %>%
 
 clipr::write_clip(missed_files_for_export)
 
-handmatched_filenames <- read_tsv("data/DataViz/Daten_WFPP/matching-missing-filmographies.tsv")
+handmatched_filenames <- read_tsv("../data/DataViz/Daten_WFPP/matching-missing-filmographies.tsv")
 
 prepared_missed_files <- df_wfpp_filmography_long_persons %>% 
   filter(filename %in% missed_files$filename) %>% 
@@ -79,11 +83,12 @@ test_diff <- setdiff(missed_files_for_export$filename, test_actors_name_match$fi
 
 df_wfpp_filmography_ids <- df_wfpp_filmography_ids__ %>% 
   bind_rows(., df_wfpp_filmography_ids__2) %>% 
-  filter(!id_person %in% c("Q346150", "Q4588079")) # durch die matching methode werden für die Nissen-Schwestern alle drei verknüpft, obwohl es nur eine Biografie für Aud gibt
+  filter(!id_person %in% c("Q346150", "Q4588079")) %>% # durch die matching methode werden für die Nissen-Schwestern alle drei verknüpft, obwohl es nur eine Biografie für Aud gibt
+  select(id_person, id, name, Permalink, filename, title = Title, actors, role, archives, release_years, production_company, production_country = Production_Country, unfinished, uncertainty)
 
 # setdiff(file_list, df_wfpp_filmography_ids$filename)
 
-write_csv(df_wfpp_filmography_ids, "data/doc/wfpp-filmography-pauline-ids.csv") 
+write_csv(df_wfpp_filmography_ids, "../data/doc/wfpp-filmography-ids.csv") 
 
 # rm(df_merged_2)
 
