c9c0df159f837c2b976f75b44df372610bbf5e68
Martina Schories <20225984+mschories@users.noreply.github.com>
2024-10-31 11:48:43 +0100

    Update config-graphic.R

--- config/config-graphic.R
+++ config/config-graphic.R
@@ -2,9 +2,9 @@ library(ggtext)
 
 # systemfonts::register_font(name="Skia-Regular_Condensed", plain = "Skia-Regular_Condensed")
 # systemfonts::register_font("Skia-Regular_Condensed")
-typo_sfb_mono <- "Monaco"
+typo_mono <- "Monaco"
 
-# typo_sfb_mono <- "Roboto Mono"
+# typo_mono <- "Roboto Mono"
 
 # gdtools::register_gfont("Roboto Mono")
 # typo_mono <- "Roboto Mono"
@@ -12,7 +12,7 @@ typo_sfb_mono <- "Monaco"
 theme_ffg_base <- theme(
     plot.title.position = "plot",
     axis.title = element_blank(),
-    axis.text = element_text(family = typo_sfb_mono, size = 10, hjust = 0),
+    axis.text = element_text(family = typo_mono, size = 10, hjust = 0),
     axis.ticks = element_blank(),
     axis.ticks.length = unit(0,"in"),
     panel.background = element_blank(),
@@ -24,20 +24,20 @@ theme_ffg_base <- theme(
     legend.justification = "center",
     legend.position = "top",
     legend.direction = "horizontal",
-    legend.title = element_text(family = typo_sfb_mono, size = 12, hjust = 0.5),
+    legend.title = element_text(family = typo_mono, size = 12, hjust = 0.5),
     legend.margin = margin(0, .2, 0, .2, "in"),
     legend.key = element_blank(),
-    text = element_text(family = typo_sfb_mono, size = 12, hjust = 0)
+    text = element_text(family = typo_mono, size = 12, hjust = 0)
   )
   
 ffg_facets <- theme(
-    strip.text = element_text(family = typo_sfb_mono, size = 11, hjust = 0),
+    strip.text = element_text(family = typo_mono, size = 11, hjust = 0),
     axis.text.y = element_blank(),
     panel.background = element_rect(fill = "#f2f2f2")
   )
 
 ffg_facets_polar <- theme(
-  strip.text = element_text(family = typo_sfb_mono, size = 10, hjust = 0),
+  strip.text = element_text(family = typo_mono, size = 10, hjust = 0),
   axis.text.y = element_blank(),
   panel.background = element_rect(fill = "#f2f2f2", color = NA),
   panel.grid.major.y = element_line(color = "#c2c2c2", linewidth = .2),
@@ -46,4 +46,14 @@ ffg_facets_polar <- theme(
 )
 
 
-  
+
+ffg_facets_occupations <- theme(
+  strip.text = element_text(family = typo_mono, size = 11, hjust = 0),
+  # axis.text.y = element_blank(),
+  panel.grid.major.y = element_line(color = "#c2c2c2", linewidth = .2),
+  panel.background = element_rect(fill = "#f2f2f2")
+) 
+
+ffg_legend_justification_left <- theme(
+  legend.location = "plot"
+)
